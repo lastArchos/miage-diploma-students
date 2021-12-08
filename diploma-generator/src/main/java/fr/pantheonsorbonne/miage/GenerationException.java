467b257e102c2ecc0664dab3fb9a3991f4b858c6
lastArchos <pilleux.raphael@gmail.com>
2021-12-08 21:44:53 +0100

    correction des problèmes sonarlint

---
+++ diploma-generator/src/main/java/fr/pantheonsorbonne/miage/GenerationException.java
@@ -0,0 +1,10 @@
+package fr.pantheonsorbonne.miage;
+
+public class GenerationException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public GenerationException(String s) {
+		super(s);
+	}
+
+}
