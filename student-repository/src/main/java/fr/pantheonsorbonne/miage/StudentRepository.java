663b5cb0d1c01833f0ea179783de5d701ccbe1eb
Pilleux Raphaël <91605434+lastArchos@users.noreply.github.com>
2021-12-09 22:41:11 +0100

    Update StudentRepository.java

--- student-repository/src/main/java/fr/pantheonsorbonne/miage/StudentRepository.java
+++ student-repository/src/main/java/fr/pantheonsorbonne/miage/StudentRepository.java
@@ -66,7 +66,7 @@ public class StudentRepository implements Iterable<Student> {
 			CSVParser parser = CSVParser.parse(reader, CSVFormat.DEFAULT);
 			this.currentIterator = parser.getRecords().stream()
 
-					.map(reccord -> new Student(Integer.parseInt(reccord.get(2)), reccord.get(0), reccord.get(1)))
+					.map(reccord -> new Student(Integer.parseInt(reccord.get(2)), reccord.get(0), reccord.get(1),""))
 					.map(c -> c).iterator();
 
 			return this.currentIterator;
